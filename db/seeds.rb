0fe5d9e23a13ff07aca778121c2c668409890bc4
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 15:15:54 -0500

    Added actual todo list

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,11 @@ require_relative '../config/application'
 require_relative '../app/models/task'
 require_relative '../todo'
 
-require 'faker'
+require 'csv'
 
-def random_todos
-  5.times do
-    Task.create :text => Faker::Lorem.sentence(word_count= 4, supplement = false, random_words_to_add = 6)
-  end
+def todos
+  todo_list = []
+  CSV.foreach('db/todos.csv','r') { |row| todo_list << Task.create(:text => row[0]) }
 end
 
-random_todos
+todos
